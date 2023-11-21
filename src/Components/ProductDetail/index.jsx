a22e7b04dc0a2791e9ae3877b31a4495aa23aff9
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2023-11-21 10:07:13 -0500

    add state to shopping card

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,16 +1,19 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from "../../Context";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useContext } from "react";
 
 const ProductDetail = () => {
-    return (
-        <aside className="flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]">
-            <div className="flex justify-between items-center p-6">
-                <h2 className='font-medium text-xl'>Details</h2>
-                <div>
-                    <XMarkIcon className="h-6 w-6t text-dark cursor-pointer"></XMarkIcon>
-                </div>
-            </div>
-        </aside>
-    )
-}
+  const context = useContext(ShoppingCartContext);
+  return (
+    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}>
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">Details</h2>
+        <div>
+          <XMarkIcon className="h-6 w-6t text-dark cursor-pointer" onClick={() => context.closeProductDetail()}></XMarkIcon>
+        </div>
+      </div>
+    </aside>
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,10 +5,16 @@ export const ShoppingCartContext = createContext()
 // eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0)
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
 
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
     const value = {
         count,
-        setCount
+        setCount,
+        isProductDetailOpen,
+        openProductDetail,
+        closeProductDetail
     }
 
     return (
